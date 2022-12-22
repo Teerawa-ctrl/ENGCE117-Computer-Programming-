8fa6b1c7d7bcdcdd1b8b914cebc2b63009cc8afe
Teerawat <82719459+Teerawa-ctrl@users.noreply.github.com>
2022-12-23 00:04:03 +0700

    Add files via upload

---
+++ ENGE03/tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+typedef struct {  
+    char *name ;
+    int age  ;
+    float gpa  ;
+    char *sex;
+}aboutme;
+
+int main(){
+    aboutme Myinfo ={"First", 18, 5.00, "Man" };
+    printf("\nShow About my self\n");
+    printf("Name:%s\n".Myinfo.name);
+    printf("Age:%d\n",Myinfo.age);
+    printf("Gpa:%f\n",Myinfo.gpa);
+    printf("Sex:%s\n",Myinfo.sex);
+    return 0;
+}
+
+
